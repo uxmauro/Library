0aa476401ec8b2303bd30c72232d1eef600d10fb
uxmauro <mauro2@Mauros-MacBook-Pro.local>
2022-10-31 00:41:41 -0400

    Adding logic to frontend

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Book Constructor
 function Book(pic, author, title, pages, read) {
     this.pic = pic,
     this.author = author 
@@ -5,10 +6,16 @@ function Book(pic, author, title, pages, read) {
     this.pages = pages 
     this.read = read
 
+    if (pic == "") {
+        this.pic = "https://unsplash.com/photos/658ElZwWXGY"
+    }
+
     this.returnBook = function(){
         console.log(author, title, pages, read)
     }
 }
+
+//Library
 
 let myLibrary = JSON.parse(localStorage.getItem("Library") || "[]");
 console.log("# of Books: " + myLibrary.length);
@@ -17,21 +24,27 @@ let i = 0;
 
 while (i < myLibrary.length) {
     console.log(myLibrary[i]);
+
     i++;
 }
 
+
+//Add Book To Library
 function addBookToLibrary() {
+    let pic = document.getElementById("Pic").value;
     let bookname = document.getElementById("BookName").value;
     let author = document.getElementById("Author").value;
     let pages = document.getElementById("Pages").value;
     let read = document.getElementById("Read").value;
     
-    myLibrary.push(new Book(author, bookname, pages, read))
+    myLibrary.push(new Book(pic, author, bookname, pages, read))
     localStorage.setItem("Library", JSON.stringify(myLibrary));
 
     }
     
 
 
-
+function lesso(){
+    console.log("testing")
+}
 
