2b0ca9461d76fdfd0c9938e53030cadd8fb5d396
uxmauro <mauro2@Mauros-MacBook-Pro.local>
2022-11-08 09:27:29 -0500

    Adding list elements with JS

--- script.js
+++ script.js
@@ -24,7 +24,6 @@ let i = 0;
 
 while (i < myLibrary.length) {
     console.log(myLibrary[i]);
-
     i++;
 }
 
@@ -68,19 +67,107 @@ function closeModal(){
     const recentImageDataUrl = localStorage.getItem("BookImage");
  
     if (recentImageDataUrl) {
-         document.querySelector("#Preview").setAttribute("src", recentImageDataUrl);
+         document.querySelector("#BookImgpreview").setAttribute("style", "background-image: url(" +  recentImageDataUrl +")");
  }
  });
 
-   /* 
-   document.querySelector("#img").addEventListener("change", function() {
-       
-      
-     
-   }); */
+ function addBookToList() {
+    const BookItem = document.createElement("div");
+    BookItem.classList.add('BookItem');
+    document.getElementById("listofBooks").appendChild(BookItem);
+    
+    const BookDetails = document.createElement("div");
+    BookDetails.classList.add('BookDetails');
+    BookItem.appendChild(BookDetails);
+
+    const BookImgName = document.createElement("div");
+    BookImgName.classList.add('BookImgName');
+    BookDetails.appendChild(BookImgName);
+
+    const BookImgpreview = document.createElement("div");
+    BookImgpreview.setAttribute('id','BookImgpreview');
+    BookImgName.appendChild(BookImgpreview);
+
+    const BookName =  document.createElement("p")
+    BookName.setAttribute("id", "BookName-List")
+    let textToAdd = document.createTextNode("Text to be added")
+    BookName.appendChild(textToAdd)
+    BookImgName.appendChild(BookName)
+
+    const Author = document.createElement("p")
+    Author.setAttribute("id", "Author-List")
+    let textToAddAuth = document.createTextNode("Text to be added")
+    Author.appendChild(textToAddAuth)
+    BookDetails.appendChild(Author)
+
+    const Pages = document.createElement("p")
+    Pages.setAttribute("id", "Pages-List")
+    let textToAddPages = document.createTextNode("# of pages")
+    Pages.appendChild(textToAddPages)
+    BookDetails.appendChild(Pages)
+
+
+    const CheckLabel = document.createElement("label")
+    const Checkbox = document.createElement("input")
+    Checkbox.setAttribute("id", "check")
+    Checkbox.type = "checkbox"
+    CheckLabel.appendChild(Checkbox)
+    BookDetails.appendChild(CheckLabel)
+
+    const ListActions = document.createElement("div")
+    ListActions.classList.add("List-Actions")
+    ListActions.innerHTML =  '<button>' + '<iconify-icon icon="fa:pencil"    style="font-size: large; color: #0d4a81" />' + ' </button>' + '<button>' + '<iconify-icon icon="fa-solid:trash-alt"    style="font-size: large; color: #0d4a81" />' + ' </buuton'
+    BookItem.appendChild(ListActions)
+    
+
+ }
+  
 
    function uploadImage() {
-    console.log('wut')
+    console.log('uploadImage')
 }
 
-   
+   /* 
+<div class="BookItem">
+
+<div class="BookDetails">
+
+                <div class="BookImgName">
+
+                <div id="BookImgpreview"></div>  <p id="BookName-List">jhdjhdjfkjdbkjbdjjdjknd</p>
+            
+                </div>
+
+        <p id="Author-List">Aygygugxuth</p>
+            <p id="Pages-List">3</p>
+    <label f
+    or="check">
+      <input type="checkbox" id="check" />
+    </label>
+</div>
+  <div class="List-Actions">
+    <button>
+      <iconify-icon
+        icon="fa:pencil"
+        style="font-size: large; color: #0d4a81"
+      />
+    </button>
+    <button>
+      <iconify-icon
+        icon="fa-solid:trash-alt"
+        style="font-size: large; color: #0d4a81"
+      />
+    </button>
+  </div>
+</div>
+</div> */
+
+
+/* 
+function addBookToList() {
+    const BookItem = document.createElement("div");
+    BookItem.classList.add('BookItem');
+    const textnode = document.createTextNode("Water");
+    BookItem.appendChild(textnode);
+    document.getElementById("listofBooks").appendChild(BookItem);
+ } */
